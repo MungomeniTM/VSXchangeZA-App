93949f56c375ce65e08ca3cc20969d003fad0b52
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-10-27 16:00:26 +0200

    Update StackNavigator.js

--- VSXchangeZA-mobile/src/navigation/stacknavigation.js
+++ VSXchangeZA-mobile/src/navigation/stacknavigation.js
@@ -1,17 +1,15 @@
-// src/navigation/StackNavigator.js
-import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
 import DashboardScreen from "../screens/DashboardScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Dashboard" component={DashboardScreen} />
     </Stack.Navigator>
   );
