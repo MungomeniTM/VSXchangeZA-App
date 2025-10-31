9de4da29cde882b19217997e0d65ddf512cf5c43
MungomeniTM <mulisa405@gmail.com>
2025-10-31 17:46:20 +0200

    modified:   VSXchangeZA-mobile/app.json 	new file:   VSXchangeZA-mobile/eas.json 	modified:   VSXchangeZA-mobile/package-lock.json 	modified:   VSXchangeZA-mobile/package.json 	modified:   VSXchangeZA-mobile/src/screens/DashboardScreen.js

--- VSXchangeZA-mobile/src/screens/DashboardScreen.js
+++ VSXchangeZA-mobile/src/screens/DashboardScreen.js
@@ -15,7 +15,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import Animated, { useSharedValue, withTiming, useAnimatedStyle } from 'react-native-reanimated';
-import { Canvas, Circle, useSharedValue as useSkiaValue, runTiming } from '@shopify/react-native-skia';
+import { Canvas, Circle, useValue, runTiming } from '@shopify/react-native-skia';
 import AnalyticsPanel from '../components/AnalyticsPanel';
 import Sidebar from '../components/Sidebar';
 import Composer from '../components/Composer';
@@ -37,11 +37,11 @@ export default function DashboardScreen() {
     sidebarOpen.value = sidebarOpen.value ? 0 : 1;
   };
 
-  // Skia Cosmic Pulse (background motion)
+  // Skia cosmic pulse
   const pulse = useValue(0);
-  React.useEffect(() => {
+  useEffect(() => {
     runTiming(pulse, 1, { duration: 2500 });
-  }, []);
+  }, [pulse]);
 
   return (
     <View style={styles.container}>
