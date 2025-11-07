574a19c801471f676983b88b9cb44c2623dbc965
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-11-07 16:09:35 +0200

    Update App.js

--- VSXchangeZA-mobile/App.js
+++ VSXchangeZA-mobile/App.js
@@ -1,15 +1,18 @@
-// App.js
+// App.js - UPDATED WITH CONTEXT PROVIDER
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator";
 import useInitializeAPI from "./src/config/initAPI";
+import { AppProvider } from "./src/context/AppContext"; // 👈 ADD THIS IMPORT
 
 export default function App() {
   useInitializeAPI(); // 👈 runs once when the app loads
 
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <AppProvider> {/* 👈 WRAP WITH CONTEXT PROVIDER */}
+        <StackNavigator />
+      </AppProvider>
     </NavigationContainer>
   );
 }
